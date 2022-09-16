92a27faffb343cabf0b954480607b39888e6529d
muccidavide <davidemucci0804@gmail.com>
2022-09-16 14:27:51 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Snack #1 utente inserisce due numeri in successione, stampare il maggiore
+
+/* Console.WriteLine("Inserisci un numero");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inserisci un altro numero");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.WriteLine("i numeri sono uguali");
+}
+else
+{
+    if (number1 > number2)
+    {
+        Console.WriteLine($"Il numero maggiore è {number1}");
+    }
+    else
+    {
+        Console.WriteLine($"Il numero maggiore è {number2}");
+    }
+}
+
+*/
+
+// Snack 2 utente inserisce due parole e il software stampa la parola piu corta
+
+/*
+
+Console.WriteLine("Inserisci una parola");
+string sentence1 = Console.ReadLine();
+
+Console.WriteLine("Inserisci una parola");
+string sentence2 = Console.ReadLine();
+
+if (sentence1.Length > sentence2.Length)
+{
+    Console.WriteLine($"La parola più lunga è {sentence1}");
+
+} else if(sentence1.Length < sentence2.Length)
+{
+    Console.WriteLine($"La parola più lunga è {sentence2}");
+}
+else
+{
+    Console.WriteLine("Le parole sono lunghe uguali");
+}
+*/
+
+// Snack #3 
+
+
+
+
+
+
+
